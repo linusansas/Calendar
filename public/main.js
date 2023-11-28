9510e29087c5d4528c671439d646eabba308ee03
Jonatan Helander <Jonatanhelander@hotmail.com>
2023-11-28 16:23:23 +0100

    Local storage, save & delete button, activity list

--- public/main.js
+++ public/main.js
@@ -5,4 +5,5 @@ function main() {
   getDayName();
   startClock();
   updateCalendar();
+  loadLocalStorage();
 }
--- public/todo.js
+++ public/todo.js
@@ -1,26 +1,48 @@
 function saveValues() {
   const titleInputFieldValue = document.getElementById('titleInputField').value;
   const timeInputFieldValue = document.getElementById('timeInputField').value;
-  const descriptionFieldValue = document.getElementById('descriptionInputField').value;
+  const dateFieldValue = document.getElementById('dateInputField').value;
+  const textareaFieldValue = document.getElementById('textareaInputField').value;
 
   const todoValues = {
       titleField: titleInputFieldValue,
       timeField: timeInputFieldValue,
-      descriptionField: descriptionFieldValue
+      dateField: dateFieldValue,
+      textareaField: textareaFieldValue
   }
+
+  const jsonStringTodo = JSON.stringify(todoValues);
+
+
+  
   localStorage.setItem('todo', JSON.stringify(todoValues))
-alert('Values saved to local storage!');
+  alert('Values saved to local storage!');
+
+  function listValues() {
+    const parsedTodoValues = JSON.parse(jsonStringTodo);
+    const listItem = document.createElement('li');
+    listItem.textContent = `Title: ${parsedTodoValues.titleField}, Time: ${parsedTodoValues.timeField}, Date: ${parsedTodoValues.dateField}, Textarea: ${parsedTodoValues.textareaField}`;
+    document.getElementById('storedItemList').appendChild(listItem);
+  }
+
+  listValues()
 }
 
 function deleteValuesAndLocalStorage() {
   document.getElementById('titleInputField').value = '';
   document.getElementById('timeInputField').value = '';
-  document.getElementById('descriptionInputField').value = '';
+  document.getElementById('dateInputField').value = '';
+  document.getElementById('textareaInputField').value = '';
 
   localStorage.removeItem('todo');
 }
-function openTodoList(year, month, day) {
-  const key = `${year}-${month + 1}-${day}`;
-  const todoListForDay = todoLists[key] || [];
-  console.log(`Todo List for ${key}:`, todoListForDay);
+function loadLocalStorage() {
+  const todoString = localStorage.getItem('todo');
+  if (todoString) {
+    const todoValues = JSON.parse(todoString);
+    document.getElementById('titleInputField').value = todoValues.titleField || '';
+    document.getElementById('timeInputField').value = todoValues.timeField || '';
+    document.getElementById('dateInputField').value = todoValues.dateField || '';
+    document.getElementById('textareaInputField').value = todoValues.textareaField || '';
+  }
 }
