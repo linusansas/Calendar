5de3febee886c9760f8facc578378d50fec49e4a
Jonatan Helander <Jonatanhelander@hotmail.com>
2023-12-07 15:53:14 +0100

    edit exports correctly to array

--- public/edit.js
+++ public/edit.js
@@ -6,36 +6,38 @@ function openEditModal() {
     const timeInput = document.getElementById("editTime");
     const textareaInput = document.getElementById("editTextarea");
   
-    const todoToEdit = todos[editingTodoIndex];
+    const todoToEdit = findTodoById(editingTodoId); // Use findTodoById to get the todo by id
     titleInput.value = todoToEdit.title;
     timeInput.value = todoToEdit.time || "";
     textareaInput.value = todoToEdit.textarea;
-  }
-  
-  function closeEditModal() {
-    const editModal = document.getElementById("editModal");
-    editModal.style.display = "none";
-  }
-
- function allowUserEdit(todoItem) {
-   // Populate modal fields with todoItem data
-   document.getElementById("editTitle").value = todoItem.title;
-   document.getElementById("editTime").value = todoItem.time || "";
-   document.getElementById("editTextarea").value = todoItem.textarea || "";
-
-   // Show the edit modal
-   const editModal = document.getElementById("editModal");
-   editModal.style.display = "block";
 }
 
-  function saveEditedTodo() {
+function closeEditModal() {
+    const editModal = document.getElementById("editModal");
+    editModal.style.display = "none";
+}
+
+function allowUserEdit(todoItem) {
+    // Populate modal fields with todoItem data
+    document.getElementById("editTitle").value = todoItem.title;
+    document.getElementById("editTime").value = todoItem.time || "";
+    document.getElementById("editTextarea").value = todoItem.textarea || "";
+
+    // Set the editingTodoId to the id of the todoItem
+    editingTodoId = todoItem.id;
+
+    // Show the edit modal
+    openEditModal();
+}
+
+function saveEditedTodo() {
     // Retrieve values from the modal
     const editedTitle = document.getElementById("editTitle").value;
     const editedTime = document.getElementById("editTime").value;
     const editedTextarea = document.getElementById("editTextarea").value;
   
     // Update the todo data
-    const editedTodo = todos[editingTodoIndex];
+    const editedTodo = findTodoById(editingTodoId); // Use findTodoById to get the todo by id
     editedTodo.title = editedTitle;
     editedTodo.time = editedTime;
     editedTodo.textarea = editedTextarea;
@@ -49,9 +51,22 @@ function openEditModal() {
   
     // Close the edit modal
     closeEditModal();
-  }
-  function closeEditModal() {
-    // Close the edit modal
-    const editModal = document.getElementById("editModal");
-    editModal.style.display = "none";
- }
+}
+
+// Declare a global variable for storing the id of the todo being edited
+let editingTodoId = null;
+
+function findTodoById(todoItemId) {
+    // Function to find a todo by its id
+    for (const todo of todos) {
+        if (todo.id === todoItemId) {
+            return todo;
+        } else if (todo.todos && Array.isArray(todo.todos)) {
+            const nestedTodo = findTodoById(todo.todos, todoItemId);
+            if (nestedTodo) {
+                return nestedTodo;
+            }
+        }
+    }
+    return null;
+}
