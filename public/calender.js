535a64b505c9bdd9e8b2c976f286f33c801e4320
CatAvadani <catalinaavadani@gmail.com>
2023-12-08 12:46:44 +0100

    Applying the previous styles,  passing all the tests.

--- public/calender.js
+++ public/calender.js
@@ -2,143 +2,149 @@ let currentMonth = new Date().getMonth();
 let currentYear = new Date().getFullYear();
 
 function createCalendar(year, month) {
-   const monthName = document.querySelector(".month-name");
-   const previousBtn = document.getElementById("previous-btn");
-   const nextBtn = document.getElementById("next-btn");
-   const daysGrid = document.querySelector(".grid-days");
-   const imageElement = document.querySelector(".month-img");
+  const monthName = document.querySelector(".month-name");
+  const previousBtn = document.getElementById("previous-btn");
+  const nextBtn = document.getElementById("next-btn");
+  const daysGrid = document.querySelector(".grid-days");
+  const imageElement = document.querySelector(".month-img");
 
-   const firstDay = new Date(year, month, 1);
-   const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDay = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-   let firstDayIndex = (firstDay.getDay() + 6) % 7;
+  let firstDayIndex = (firstDay.getDay() + 6) % 7;
 
-   const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-   ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-   //   changeMonthImage(imageElement, month);
+  changeMonthImage(imageElement, month);
 
-   monthName.textContent = `${monthNames[month]} ${year}`;
-   daysGrid.innerHTML = "";
-   nextBtn.onclick = nextMonth;
-   previousBtn.onclick = prevMonth;
+  monthName.textContent = `${monthNames[month]} ${year}`;
+  daysGrid.innerHTML = "";
+  nextBtn.onclick = nextMonth;
+  previousBtn.onclick = prevMonth;
 
-   for (let i = 0; i < firstDayIndex; i++) {
-      const dayElement = document.createElement("p");
-      dayElement.className = "day";
-      daysGrid.appendChild(dayElement);
-      dayElement.setAttribute("data-cy", "calendar-cell");
-      const textElement = document.createElement("div");
-      dayElement.appendChild(textElement);
-      textElement.setAttribute("data-cy", "calendar-cell-date");
-   }
+  for (let i = 0; i < firstDayIndex; i++) {
+    const dayElement = document.createElement("p");
+    dayElement.className = "empty-day";
+    daysGrid.appendChild(dayElement);
+    dayElement.setAttribute("data-cy", "calendar-cell");
+    const textElement = document.createElement("div");
+    dayElement.appendChild(textElement);
+    textElement.setAttribute("data-cy", "calendar-cell-date");
+  }
 
-   for (let i = 1; i <= daysInMonth; i++) {
-      const dayElement = document.createElement("div");
-      dayElement.className = "day";
-      dayElement.setAttribute("data-cy", "calendar-cell");
-      dayElement.setAttribute("onclick", "selectDayTodo(event)");
-      dayElement.setAttribute("id", `day${i}`);
+  for (let i = 1; i <= daysInMonth; i++) {
+    const dayElement = document.createElement("div");
+    dayElement.className = "day";
+    dayElement.setAttribute("data-cy", "calendar-cell");
+    dayElement.setAttribute("onclick", "selectDayTodo(event)");
+    dayElement.setAttribute("id", `day${i}`);
 
-      const textElement = document.createElement("div");
-      textElement.textContent = i;
-      textElement.setAttribute("data-cy", "calendar-cell-date");
-      textElement.setAttribute("onclick", "selectDayTodo(event)");
-      dayElement.appendChild(textElement);
+    const textElement = document.createElement("div");
+    textElement.textContent = i;
+    textElement.setAttribute("data-cy", "calendar-cell-date");
+    textElement.setAttribute("onclick", "selectDayTodo(event)");
+    dayElement.appendChild(textElement);
 
-      if (
-         i === new Date().getDate() &&
-         year === new Date().getFullYear() &&
-         month === new Date().getMonth()
-      ) {
-         dayElement.classList.add("today");
+    if (
+      i === new Date().getDate() &&
+      year === new Date().getFullYear() &&
+      month === new Date().getMonth()
+    ) {
+      dayElement.classList.add("today");
+    } else {
+      const dayOfWeek = (firstDayIndex + i - 1) % 7;
+      if (dayOfWeek === 5 || dayOfWeek === 6) {
+        dayElement.classList.add("weekend");
       }
+    }
 
-      const cellDate = new Date(year, month, i + 1);
-      const cellDateString = cellDate.toISOString().split("T")[0];
-      const todosForDate = todos.filter(
-         (todoItem) => todoItem.date === cellDateString
-      );
+    const cellDate = new Date(year, month, i + 1);
+    const cellDateString = cellDate.toISOString().split("T")[0];
+    const todosForDate = todos.filter(
+      (todoItem) => todoItem.date === cellDateString
+    );
 
-      if (todosForDate.length) {
-         const todoCountElement = document.createElement("div");
-         todoCountElement.setAttribute("data-cy", "calendar-cell-todos");
-         todoCountElement.textContent = todosForDate.length;
-         dayElement.appendChild(todoCountElement);
-      }
+    if (todosForDate.length) {
+      const todoCountElement = document.createElement("div");
+      todoCountElement.className = "todo-count";
+      todoCountElement.setAttribute("data-cy", "calendar-cell-todos");
+      todoCountElement.textContent = todosForDate.length;
+      dayElement.appendChild(todoCountElement);
+    }
 
-      daysGrid.appendChild(dayElement);
-   }
+    daysGrid.appendChild(dayElement);
+  }
 }
 
 function updateCalendar() {
-   createCalendar(currentYear, currentMonth);
+  createCalendar(currentYear, currentMonth);
 }
 
 function nextMonth() {
-   currentMonth = (currentMonth + 1) % 12;
-   if (currentMonth === 0) {
-      currentYear++;
-   }
-   updateCalendar();
+  currentMonth = (currentMonth + 1) % 12;
+  if (currentMonth === 0) {
+    currentYear++;
+  }
+  updateCalendar();
 }
 
 function prevMonth() {
-   currentMonth = (currentMonth - 1 + 12) % 12;
-   if (currentMonth === 11) {
-      currentYear--;
-   }
-   updateCalendar();
+  currentMonth = (currentMonth - 1 + 12) % 12;
+  if (currentMonth === 11) {
+    currentYear--;
+  }
+  updateCalendar();
 }
 
-// /**
-//  * Change the calendar image depending on the month
-//  * @param {*} imageElement
-//  * @param {*} month
-//  */
-// function changeMonthImage(imageElement, month) {
-//   const monthImg = [
-//     "./images/january-img.png",
-//     "./images/february-img.jpeg",
-//     "./images/march-img.webp",
-//     "./images/april-img.jpeg",
-//     "./images/may-img.webp",
-//     "./images/june-img.jpeg",
-//     "./images/july-img.jpeg",
-//     "./images/august-img.jpeg",
-//     "./images/september-img.jpeg",
-//     "./images/october-img.jpeg",
-//     "./images/november-img.jpeg",
-//     "./images/december-img.jpeg",
-//   ];
+/**
+ * Change the calendar image depending on the month
+ * @param {*} imageElement
+ * @param {*} month
+ */
+function changeMonthImage(imageElement, month) {
+  const monthImg = [
+    "./images/january-img.png",
+    "./images/february-img.jpeg",
+    "./images/march-img.webp",
+    "./images/april-img.jpeg",
+    "./images/may-img.webp",
+    "./images/june-img.jpeg",
+    "./images/july-img.jpeg",
+    "./images/august-img.jpeg",
+    "./images/september-img.jpeg",
+    "./images/october-img.jpeg",
+    "./images/november-img.jpeg",
+    "./images/december-img.jpeg",
+  ];
 
-//   const indexImg = month;
-//   imageElement.setAttribute("src", monthImg[indexImg]);
-// }
+  const indexImg = month;
+  imageElement.setAttribute("src", monthImg[indexImg]);
+}
 
 function handleDateChange() {
-   const selectedDate = document.getElementById("dateInputField").value;
+  const selectedDate = document.getElementById("dateInputField").value;
 
-   const parsedDate = new Date(selectedDate);
+  const parsedDate = new Date(selectedDate);
 
-   currentMonth = parsedDate.getMonth();
-   currentYear = parsedDate.getFullYear();
+  currentMonth = parsedDate.getMonth();
+  currentYear = parsedDate.getFullYear();
 
-   getDayName(parsedDate.getDate(), currentMonth, currentYear);
+  getDayName(parsedDate.getDate(), currentMonth, currentYear);
 
-   updateCalendar();
-   clearTodoList();
-   updateTodoList();
+  updateCalendar();
+  clearTodoList();
+  updateTodoList();
 }
--- public/todo.js
+++ public/todo.js
@@ -4,243 +4,249 @@ let editingTodoIndex = -1;
 let originalTodo = null;
 
 function saveValues() {
-   const dateUserInput = document.getElementById("dateInputField").value;
-   const timeUserInput = document.getElementById("timeInputField").value;
-   const titleUserInput = document.getElementById("titleInputField").value;
-   const textareaUserInput =
-      document.getElementById("textareaInputField").value;
+  const dateUserInput = document.getElementById("dateInputField").value;
+  const timeUserInput = document.getElementById("timeInputField").value;
+  const titleUserInput = document.getElementById("titleInputField").value;
+  const textareaUserInput = document.getElementById("textareaInputField").value;
 
-   return {
-      date: dateUserInput,
-      time: timeUserInput,
-      title: titleUserInput,
-      textarea: textareaUserInput,
-   };
+  return {
+    date: dateUserInput,
+    time: timeUserInput,
+    title: titleUserInput,
+    textarea: textareaUserInput,
+  };
 }
 
 function createTodoElement(todoData) {
-   const newTodo = document.createElement("li");
+  const newTodo = document.createElement("li");
+  newTodo.className = "activity-card";
 
-   const todoText = `
-    <span>Title: ${todoData.title}</span><br>
-    <span>Date: ${todoData.date}</span><br>
-    <span>Time: ${todoData.time}</span><br>
-    <span>Textarea: ${todoData.textarea}</span>`;
+  const todoText = `
+  <span class="dateField" >${todoData.date}</span><br>
+  <span class="timeField"> ${todoData.time}</span><br>
+  <span class="todo-title" >${todoData.title}</span><br>
+    <span class="textarea">${todoData.textarea}</span>`;
 
-   newTodo.innerHTML = todoText;
+  newTodo.innerHTML = todoText;
 
-   const deleteButton = document.createElement("button");
-   deleteButton.textContent = "Delete";
-   deleteButton.onclick = () => deleteTodo(todoData.date, todoData.id);
-   deleteButton.setAttribute("data-cy", "delete-todo-button");
-   newTodo.appendChild(deleteButton);
+  // Actions Section
+  const actionsSection = document.createElement("div");
+  actionsSection.className = "activity-actions";
 
-   const editButton = document.createElement("button");
-   editButton.textContent = "Edit";
-   editButton.setAttribute("data-cy", "edit-todo-button");
-   editButton.onclick = () => allowUserEdit(todoData);
-   editButton.setAttribute("data-edit-id", todoData.id);
-   newTodo.appendChild(editButton);
-   editButton.setAttribute("id", "editButton")
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("data-cy", "delete-todo-button");
+  deleteButton.textContent = "Delete";
+  deleteButton.onclick = () => deleteTodo(todoData.date, todoData.id);
+  actionsSection.appendChild(deleteButton);
 
-   return newTodo;
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.setAttribute("data-cy", "edit-todo-button");
+  editButton.onclick = () => allowUserEdit(todoData);
+  editButton.setAttribute("data-edit-id", todoData.id);
+  actionsSection.appendChild(editButton);
+  editButton.setAttribute("id", "editButton");
+
+  newTodo.appendChild(actionsSection);
+
+  return newTodo;
 }
 
 function createTodo() {
-   const userInputData = saveValues();
-   const editButton = document.getElementById("editButton");
-   const editId = editButton ? editButton.getAttribute("data-edit-id") : null;
+  const userInputData = saveValues();
+  const editButton = document.getElementById("editButton");
+  const editId = editButton ? editButton.getAttribute("data-edit-id") : null;
 
-   if (editId) {
-      // Editing an existing todo
-      const editingTodoIndex = todos.findIndex((todoItem) => todoItem.id === editId);
+  if (editId) {
+    // Editing an existing todo
+    const editingTodoIndex = todos.findIndex(
+      (todoItem) => todoItem.id === editId
+    );
 
-      if (editingTodoIndex !== -1) {
-         // Update the existing todo with the edited data
-         todos[editingTodoIndex] = { ...userInputData, id: editId };
-         localStorage.setItem("todos", JSON.stringify(todos));
-         updateCalendar();
-         clearInputFields();
-         updateTodoList();
-      } else {
-         console.error("Todo not found for the given id:", editId);
-      }
-   } else {
-      // Creating a new todo
-      const newTodoId = getNextUniqueId();
-      const newTodo = { ...userInputData, id: newTodoId };
-
-      todos.push(newTodo);
+    if (editingTodoIndex !== -1) {
+      // Update the existing todo with the edited data
+      todos[editingTodoIndex] = { ...userInputData, id: editId };
       localStorage.setItem("todos", JSON.stringify(todos));
       updateCalendar();
       clearInputFields();
       updateTodoList();
-      const todoElement = createTodoElement(newTodo);
-      document.getElementById("todoList").appendChild(todoElement);
-   }
+    } else {
+      console.error("Todo not found for the given id:", editId);
+    }
+  } else {
+    // Creating a new todo
+    const newTodoId = getNextUniqueId();
+    const newTodo = { ...userInputData, id: newTodoId };
 
-   console.log(todos);
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    updateCalendar();
+    clearInputFields();
+    updateTodoList();
+    const todoElement = createTodoElement(newTodo);
+    document.getElementById("todoList").appendChild(todoElement);
+  }
+
+  console.log(todos);
 }
 
 function deleteTodo(date, todoId) {
-   const todoIndex = todos.findIndex(
-      (todoItem) => todoItem.date === date && todoItem.id === todoId
-   );
+  const todoIndex = todos.findIndex(
+    (todoItem) => todoItem.date === date && todoItem.id === todoId
+  );
 
-   if (todoIndex !== -1) {
-      todos.splice(todoIndex, 1);
-      localStorage.setItem("todos", JSON.stringify(todos));
-      updateCalendar();
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    updateCalendar();
 
-      clearTodoList();
+    clearTodoList();
 
-      const todosForSelectedDate = todos.filter(
-         (todoItem) => todoItem.date === date
-      );
-      todosForSelectedDate.forEach((todoItem) => {
-         const todoElement = createTodoElement(todoItem);
-         document.getElementById("todoList").appendChild(todoElement);
-      });
-   } else {
-      console.error("Todo not found for the given date and id:", date, todoId);
-   }
-} 
+    const todosForSelectedDate = todos.filter(
+      (todoItem) => todoItem.date === date
+    );
+    todosForSelectedDate.forEach((todoItem) => {
+      const todoElement = createTodoElement(todoItem);
+      document.getElementById("todoList").appendChild(todoElement);
+    });
+  } else {
+    console.error("Todo not found for the given date and id:", date, todoId);
+  }
+}
 function storeTodo(callback) {
-   if (todos && Array.isArray(todos)) {
-      const hasNonEmptyTodo = todos.some(
-         (item) =>
-            item.date !== "" ||
-            item.time !== "" ||
-            item.title !== "" ||
-            item.textarea !== ""
-      );
+  if (todos && Array.isArray(todos)) {
+    const hasNonEmptyTodo = todos.some(
+      (item) =>
+        item.date !== "" ||
+        item.time !== "" ||
+        item.title !== "" ||
+        item.textarea !== ""
+    );
 
-      let todoString = JSON.stringify(todos);
-      localStorage.setItem("todos", todoString);
+    let todoString = JSON.stringify(todos);
+    localStorage.setItem("todos", todoString);
 
-      if (callback && typeof callback === "function") {
-         callback();
-      }
-   } else {
-      console.error("Invalid or uninitialized todos");
-   }
+    if (callback && typeof callback === "function") {
+      callback();
+    }
+  } else {
+    console.error("Invalid or uninitialized todos");
+  }
 }
 
 function clearTodoList() {
-   const todoListElement = document.getElementById("todoList");
+  const todoListElement = document.getElementById("todoList");
 
-   while (todoListElement.firstChild) {
-      todoListElement.removeChild(todoListElement.firstChild);
-   }
+  while (todoListElement.firstChild) {
+    todoListElement.removeChild(todoListElement.firstChild);
+  }
 }
 
 function clearInputFields() {
-   const dateInputField = document.getElementById("dateInputField");
-   const timeInputField = document.getElementById("timeInputField");
-   const titleInputField = document.getElementById("titleInputField");
-   const textareaInputField = document.getElementById("textareaInputField");
+  const dateInputField = document.getElementById("dateInputField");
+  const timeInputField = document.getElementById("timeInputField");
+  const titleInputField = document.getElementById("titleInputField");
+  const textareaInputField = document.getElementById("textareaInputField");
 
-   if (dateInputField) {
-      dateInputField.value = "";
-   } else {
-      console.error("dateInputField not found in the DOM");
-   }
+  if (dateInputField) {
+    dateInputField.value = "";
+  } else {
+    console.error("dateInputField not found in the DOM");
+  }
 
-   if (timeInputField) {
-      timeInputField.value = "";
-   } else {
-      console.error("timeInputField not found in the DOM");
-   }
+  if (timeInputField) {
+    timeInputField.value = "";
+  } else {
+    console.error("timeInputField not found in the DOM");
+  }
 
-   if (titleInputField) {
-      titleInputField.value = "";
-   } else {
-      console.error("titleInputField not found in the DOM");
-   }
+  if (titleInputField) {
+    titleInputField.value = "";
+  } else {
+    console.error("titleInputField not found in the DOM");
+  }
 
-   if (textareaInputField) {
-      textareaInputField.value = "";
-   } else {
-      console.error("textareaInputField not found in the DOM");
-   }
+  if (textareaInputField) {
+    textareaInputField.value = "";
+  } else {
+    console.error("textareaInputField not found in the DOM");
+  }
 }
 
 function updateTodoList(selectedDate) {
-   const todoListElement = document.getElementById("todoList");
-   todoListElement.innerHTML = "";
-   const existingTodos = todos.filter(
-      (todoItem) => todoItem.date === selectedDate
-   );
-   existingTodos.forEach((todoItem) => {
-      const injectTodo = createTodoElement(todoItem, todoItem.id);
-      todoListElement.appendChild(injectTodo);
-   });
+  const todoListElement = document.getElementById("todoList");
+  todoListElement.innerHTML = "";
+  const existingTodos = todos.filter(
+    (todoItem) => todoItem.date === selectedDate
+  );
+  existingTodos.forEach((todoItem) => {
+    const injectTodo = createTodoElement(todoItem, todoItem.id);
+    todoListElement.appendChild(injectTodo);
+  });
 }
 
-
 function findTodoById(todoItemId) {
-   function findTodo(todoList) {
-      for (const todo of todoList) {
-         if (todo.id === todoItemId) {
-            return todo;
-         } else if (todo.todos && Array.isArray(todo.todos)) {
-            const nestedTodo = findTodo(todo.todos);
-            if (nestedTodo) {
-               return nestedTodo;
-            }
-         }
+  function findTodo(todoList) {
+    for (const todo of todoList) {
+      if (todo.id === todoItemId) {
+        return todo;
+      } else if (todo.todos && Array.isArray(todo.todos)) {
+        const nestedTodo = findTodo(todo.todos);
+        if (nestedTodo) {
+          return nestedTodo;
+        }
       }
-      return null;
-   }
+    }
+    return null;
+  }
 
-   return findTodo(todos);
+  return findTodo(todos);
 }
 
 function selectDayTodo(event) {
-   const selectedDayElement = event.target.closest(".day");
-   if (!selectedDayElement) return;
+  const selectedDayElement = event.target.closest(".day");
+  if (!selectedDayElement) return;
 
-   const selectedDayId = selectedDayElement.id;
-   const selectedDayNumber = parseInt(selectedDayId.replace("day", ""), 10);
+  const selectedDayId = selectedDayElement.id;
+  const selectedDayNumber = parseInt(selectedDayId.replace("day", ""), 10);
 
-   const selectedMonth = currentMonth;
-   const selectedYear = currentYear;
-   const selectedDate = `${selectedYear}-${(selectedMonth + 1)
-      .toString()
-      .padStart(2, "0")}-${selectedDayNumber.toString().padStart(2, "0")}`;
+  const selectedMonth = currentMonth;
+  const selectedYear = currentYear;
+  const selectedDate = `${selectedYear}-${(selectedMonth + 1)
+    .toString()
+    .padStart(2, "0")}-${selectedDayNumber.toString().padStart(2, "0")}`;
 
-   const dateInputField = document.getElementById("dateInputField");
-   dateInputField.value = selectedDate;
+  const dateInputField = document.getElementById("dateInputField");
+  dateInputField.value = selectedDate;
 
-   getDayName(selectedDayNumber, selectedMonth, selectedYear);
-   currentMonth = selectedMonth;
-   currentYear = selectedYear;
+  getDayName(selectedDayNumber, selectedMonth, selectedYear);
+  currentMonth = selectedMonth;
+  currentYear = selectedYear;
 
-   clearTodoList();
+  clearTodoList();
 
-   updateCalendar();
+  updateCalendar();
 
-   const todosForSelectedDate = todos.filter(
-      (todoItem) => todoItem.date === selectedDate
-   );
-   if (todosForSelectedDate.length > 0) {
-      todosForSelectedDate.forEach((todoItem) => {
-         const injectTodo = createTodoElement(todoItem, todoItem.id);
-         document.getElementById("todoList").appendChild(injectTodo);
-      });
-   } else {
-      updateTodoList(selectedDate);
-   }
+  const todosForSelectedDate = todos.filter(
+    (todoItem) => todoItem.date === selectedDate
+  );
+  if (todosForSelectedDate.length > 0) {
+    todosForSelectedDate.forEach((todoItem) => {
+      const injectTodo = createTodoElement(todoItem, todoItem.id);
+      document.getElementById("todoList").appendChild(injectTodo);
+    });
+  } else {
+    updateTodoList(selectedDate);
+  }
 
-   console.log(todos);
+  console.log(todos);
 }
 
-
 function getNextUniqueId() {
-   const existingTodo = todos.find(todo => todo.id === todos.length + 1);
-   if (existingTodo) {
-      return existingTodo.id.toString();
-   }
+  const existingTodo = todos.find((todo) => todo.id === todos.length + 1);
+  if (existingTodo) {
+    return existingTodo.id.toString();
+  }
 
-   return (todos.length + 1).toString();
+  return (todos.length + 1).toString();
 }
